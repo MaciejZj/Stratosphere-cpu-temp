ee1abfa5e716b2c5ac10e2372b05e3592bdb8748
Maciej Ziaja <maciejzjg@gmail.com>
2019-06-11 17:50:18 +0200

    Implement basic app exec loop function

--- include/application.hpp
+++ include/application.hpp
@@ -25,4 +25,29 @@ public:
 	void init_signals() {
 		std::signal(SIGTERM, Application::termination_handler);
 	}
+	
+	int exec() {
+		string cpu_temp;
+		
+		init_log();
+		init_signals();
+		spdlog::info("Starting app...");
+		
+		myfile.open("example.txt", std::fstream::in);
+		
+		while (running) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			if (cpu_temp_file) {
+				getline(cpu_temp_file, cpu_temp);
+				// TODO: Parse temp and send it to socket
+			} else {
+				spdlog::critical(cpu_temp_file.rdstate());
+				running = false;
+			}
+		}
+		
+		cpu_temp_file.close();
+		return 0;
+	}
+
 };
