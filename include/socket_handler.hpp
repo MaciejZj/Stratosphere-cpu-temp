6569f9a9292c1ed6a0bee4f78e96c3f04793a8cc
Maciej Ziaja <maciejzjg@gmail.com>
2019-06-12 19:09:28 +0200

    Add error classes to socket handler header

--- include/socket_handler.hpp
+++ include/socket_handler.hpp
@@ -3,6 +3,16 @@
 #include <string>
 #include "gpsframe.hpp"
 
+class config_error : public std::runtime_error {
+	public:
+		config_error(const char* what_arg) : std::runtime_error(what_arg) {}
+	};
+
+class network_error : public std::runtime_error {
+	public:
+		network_error(const char* what_arg) : std::runtime_error(what_arg) {}
+	};
+
 class Socket_handler {
 	private:
 		zmq::socket_t* socket;
