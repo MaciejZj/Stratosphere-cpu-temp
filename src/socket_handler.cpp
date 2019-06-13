9deb0d6e2208e80566b9bcf93a5aff95c58be701
Maciej Ziaja <maciejzjg@gmail.com>
2019-06-13 11:08:04 +0200

    Delete errors namespaces

--- src/socket_handler.cpp
+++ src/socket_handler.cpp
@@ -6,18 +6,18 @@ Socket_handler::Socket_handler() {
 	try {
 		cfg.readFile("/etc/sp/sp.cfg");
 	} catch (const libconfig::FileIOException &fioex) {
-		throw ublox::config_error("I/O error while reading file.");
+		throw config_error("I/O error while reading file.");
 	} catch (const libconfig::ParseException &pex) {
 		std::stringstream ss;
 		ss << "Parse error at " << pex.getFile() << ":" << pex.getLine()
 				  << " - " << pex.getError() << std::endl;
-		throw ublox::config_error(ss.str().c_str());
+		throw config_error(ss.str().c_str());
 	}
 
 	const libconfig::Setting& root = cfg.getRoot();
 	int port;
 	if (!root["cpu_temp"].lookupValue("cpu_temp_port", port)) {
-		throw ublox::config_error("CPU temperature file not defined in config.");
+		throw config_error("CPU temperature file not defined in config.");
 	}
 	spdlog::info("Config parsed.");
 
@@ -27,10 +27,10 @@ Socket_handler::Socket_handler() {
 		if (socket != nullptr) {
 			socket->bind("tcp://*:" + std::to_string(port));
 		} else {
-			throw ublox::network_error("Error while creating socket.");
+			throw network_error("Error while creating socket.");
 		}
 	} catch (const zmq::error_t &e) {
-		throw ublox::network_error(e.what());
+		throw network_error(e.what());
 	}
 	spdlog::info("Socket opened at: {}", port);
 }
@@ -67,7 +67,7 @@ void publish(std::string data) {
 	auto frame = process_data(data);
 	try {
 		send_data(frame);
-	} catch(const network_error& e) {
+	} catch (const network_error& e) {
 		spdlog::error(e.what());
 	}
 }
