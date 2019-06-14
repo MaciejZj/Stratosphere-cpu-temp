9aa57a38ca0f25b122da42ceb16909fa53b27f28
Maciej Ziaja <maciejzjg@gmail.com>
2019-06-14 15:30:58 +0200

    Create header for temp file wrapper

---
+++ include/cpu_temp_reader.hpp
@@ -0,0 +1,15 @@
+#include "cpu_temp_frame.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class Cpu_temp_reader {
+	public:
+		Cpu_temp_reader(std::string file_path);
+		~Cpu_temp_reader();
+		cpu_temp_frame_t read();
+		
+	private:
+		std::ifstream cpu_temp_file;
+		inline static cpu_temp_frame_t build_frame(std::string data);
+};
