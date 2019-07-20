41536b0357c42fda279e936bddb8fa3418fd1135
Maciej Ziaja <maciejzjg@gmail.com>
2019-07-20 17:05:41 +0200

    Make io exceptions more verbose

--- src/cpu_temp_reader.cpp
+++ src/cpu_temp_reader.cpp
@@ -1,9 +1,17 @@
 #include "cpu_temp_reader.hpp"
 #include "spdlog/spdlog.h"
+#include <sstream>
 
 Cpu_temp_reader::Cpu_temp_reader(std::string file_path) {
 	cpu_temp_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	cpu_temp_file.open(file_path);
+	try {
+		cpu_temp_file.open(file_path);
+	} catch (const std::runtime_error &e) {
+		std::stringstream err_msg;
+		err_msg << "Could not open cpu temperature file at: " << file_path
+		        << ", " << e.what();
+		throw(std::runtime_error(err_msg.str()));
+	}
 }
 
 Cpu_temp_reader::~Cpu_temp_reader() {
@@ -12,7 +20,13 @@ Cpu_temp_reader::~Cpu_temp_reader() {
 
 cpu_temp_frame_t Cpu_temp_reader::read() {
 	std::string readout;
-	cpu_temp_file >> readout;
+	try {
+		cpu_temp_file >> readout;
+	} catch (const std::runtime_error &e) {
+		std::stringstream err_msg;
+		err_msg << "Could not read cpu temperature file, " << e.what();
+		throw(std::runtime_error(err_msg.str()));
+	}
 	cpu_temp_file.seekg(0, std::ios::beg);
 	return build_frame(readout);
 }
